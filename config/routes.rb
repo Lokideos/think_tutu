1ddac8669383c69bb6ea239176b279cb7d44ef95
lokideos <archmage09@gmail.com>
2017-04-23 21:28:20 +0300

    Add routes MVC

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
+  resources :trains, :railway_stations, :routes
 
-  resources :trains
-  resources :railway_stations
+  get 'welcome/index'
 
   root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
