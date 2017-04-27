2be0f77e604c0b2f3aa0ef0a0a011dd9ad50361b
George <archmage09@gmail.com>
2017-04-27 13:28:33 +0300

    Fix foreign_key syntax problem

--- app/models/railway_station.rb
+++ app/models/railway_station.rb
@@ -1,8 +1,8 @@
 class RailwayStation < ApplicationRecord
   has_many :trains
   
-  has_many :first_station_tickets, class_name: "Ticket", foreign_key :first_station_ticket_id
-  has_many :last_station_tickets, class_name: "Ticket", foreign_key :last_station_ticket_id
+  has_many :first_station_tickets, class_name: "Ticket", foreign_key: :first_station_ticket_id
+  has_many :last_station_tickets, class_name: "Ticket", foreign_key: :last_station_ticket_id
   
   has_many :railway_stations_routes
   has_many :routes, through: :railway_stations_routes
