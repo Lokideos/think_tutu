22bce0a6828847576221e03a018b2471ceff95cb
George <archmage09@gmail.com>
2017-04-27 11:16:00 +0300

    Change association type

--- app/models/railway_station.rb
+++ app/models/railway_station.rb
@@ -1,6 +1,7 @@
 class RailwayStation < ApplicationRecord
   has_many :trains
-  has_many :tickets
+  # has_many :tickets
+  has_and_belongs_to_many :tickets
   # has_many :railway_stations_tickets
   has_many :railway_stations_routes
   has_many :routes, through: :railway_stations_routes
