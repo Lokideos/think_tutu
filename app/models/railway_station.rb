ceda6c929f3c4343df48e83d4addd1262eb0767b
George <archmage09@gmail.com>
2017-04-27 13:27:58 +0300

    Change association type to has_many

--- app/models/railway_station.rb
+++ app/models/railway_station.rb
@@ -1,11 +1,11 @@
 class RailwayStation < ApplicationRecord
   has_many :trains
-  # has_many :tickets
-  has_and_belongs_to_many :tickets
-  # has_many :railway_stations_tickets
+  
+  has_many :first_station_tickets, class_name: "Ticket", foreign_key :first_station_ticket_id
+  has_many :last_station_tickets, class_name: "Ticket", foreign_key :last_station_ticket_id
+  
   has_many :railway_stations_routes
   has_many :routes, through: :railway_stations_routes
-  # has_many :tickets, through: :railway_stations_tickets
-
+  
   validates :title, presence: true
 end
