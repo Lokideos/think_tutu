56860e8cd5b02dabb3d981b1e73640b7b560fe7b
lokideos <archmage09@gmail.com>
2017-05-01 21:12:39 +0300

    Fix association problem

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,8 +1,8 @@
 class Ticket < ApplicationRecord
-  belongs_to :user
-  belongs_to :train
-  belongs_to :first_station, class_name: "RailwayStation", foreign_key: :first_station_id
-  belongs_to :last_station, class_name: "RailwayStation", foreign_key: :last_station_id
+  belongs_to :user, optional: true
+  belongs_to :train, optional: true
+  belongs_to :first_station, class_name: "RailwayStation", foreign_key: :first_station_id, optional: true
+  belongs_to :last_station, class_name: "RailwayStation", foreign_key: :last_station_id, optional: true
   # has_many :railway_stations_tickets
   # has_many :railway_stations, through: :railway_stations_tickets
 
