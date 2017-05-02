b2bfb9006d111f740f1a1d2bce1d91d227287873
lokideos <archmage09@gmail.com>
2017-05-03 00:03:05 +0300

    Car views and welcome to cars link

--- app/models/car.rb
+++ app/models/car.rb
@@ -2,4 +2,9 @@ class Car < ApplicationRecord
   belongs_to :train
 
   validates :number, :top_seats, :bottom_seats, presence: :true
+  validate :check_car_type
+
+  def check_car_type
+    errors.add(:car_type, "Car type should be 'Cattle' or 'Coupe'") unless self.car_type == "Cattle" && "Coupe"
+  end
 end
