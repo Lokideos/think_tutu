8cacbeea91dc238e655534bb04dcb15135332457
lokideos <archmage09@gmail.com>
2017-05-03 20:46:19 +0300

    Move cars and seats calcuations from view to model

--- app/models/train.rb
+++ app/models/train.rb
@@ -4,5 +4,23 @@ class Train < ApplicationRecord
   has_many :tickets
   has_many :cars
 
+
+  def cars_quantity_by_type (chosen_type)
+    quantity = 0
+    self.cars.each do |car|
+      quantity += 1 if car.car_type == chosen_type
+    end
+    quantity
+  end
+
+  def seats_by_car_and_seat_types (chosen_car_type, seat_type)
+    quantity = 0
+    self.cars.each do |car|
+      quantity += car.bottom_seats if car.car_type == chosen_car_type && seat_type == "bottom_seats"
+      quantity += car.top_seats if car.car_type == chosen_car_type && seat_type == "top_seats"
+    end
+    quantity
+  end  
+
   validates :name, :number, presence: true
 end
