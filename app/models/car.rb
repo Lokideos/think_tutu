1c3c4bbdac3a0544dc9974943d8979249345bd1e
lokideos <archmage09@gmail.com>
2017-05-03 23:49:04 +0300

    Change custom validation to rails validation

--- app/models/car.rb
+++ app/models/car.rb
@@ -2,11 +2,5 @@ class Car < ApplicationRecord
   belongs_to :train
 
   validates :number, :top_seats, :bottom_seats, presence: :true
-  validate :check_car_type
-
-  private
-
-  def check_car_type
-    errors.add(:car_type, "Car type should be 'Cattle' or 'Coupe'") unless self.car_type == "Cattle" || self.car_type == "Coupe"
-  end
+  validates :car_type, inclusion: { in: %w(Cattle Coupe), message: "%{value} is not a valid car type"}  
 end
