6ec486b9c39a1e1cc2908dea0ab8833b085babbd
lokideos <archmage09@gmail.com>
2017-06-01 21:37:59 +0300

    Move finding stations in route logic to models

--- app/models/railway_stations_route.rb
+++ app/models/railway_stations_route.rb
@@ -4,5 +4,15 @@ class RailwayStationsRoute < ApplicationRecord
 
   validates :station_in_route_id, uniqueness: { scope: :route_id }
 
+  before_save :set_position_in_route
+
   scope :ordered, -> { order(:station_in_route_id) }
+
+  def set_position_in_route
+    if RailwayStationsRoute.all.where(["railway_stations_routes.route_id = ?", self.route_id]).ordered.last.nil?
+      self.station_in_route_id = 1
+    else
+      self.station_in_route_id = RailwayStationsRoute.all.where(["railway_stations_routes.route_id = ?", self.route_id]).ordered.last.station_in_route_id + 1
+    end
+  end
 end
--- app/models/route.rb
+++ app/models/route.rb
@@ -3,5 +3,15 @@ class Route < ApplicationRecord
   has_many :railway_stations, through: :railway_stations_routes
   has_many :trains
 
+  def stations_in_route
+    stations = []
+    RailwayStationsRoute.all.where(["railway_stations_routes.route_id = ?", self.id]).ordered.each do |association|
+      RailwayStation.all.where(["railway_stations.id = ?", association.railway_station_id]).each do |station|
+        stations << station
+      end
+    end
+    stations
+  end
+
   validates :name, presence: true
 end
