3c59d42568841080da9478ed5543f764172a7aad
lokideos <archmage09@gmail.com>
2017-06-02 19:55:24 +0300

    Modife picking up needed statoins for routes procedure

--- app/models/route.rb
+++ app/models/route.rb
@@ -3,14 +3,8 @@ class Route < ApplicationRecord
   has_many :railway_stations, through: :railway_stations_routes
   has_many :trains
 
-  def stations_in_route
-    stations = []
-    RailwayStationsRoute.all.where(['railway_stations_routes.route_id = ?', id]).ordered.each do |association|
-      RailwayStation.all.where(['railway_stations.id = ?', association.railway_station_id]).each do |station|
-        stations << station
-      end
-    end
-    stations
+  def stations_in_route    
+    self.railway_stations_routes.ordered    
   end
 
   validates :name, presence: true
