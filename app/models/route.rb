bcb9f9d1f1fee2dc606c138177e4e3eb88bc9165
lokideos <archmage09@gmail.com>
2017-06-16 16:00:20 +0300

    Simplify route search query

--- app/models/route.rb
+++ app/models/route.rb
@@ -1,25 +1,12 @@
 class Route < ApplicationRecord
   has_many :railway_stations_routes
   has_many :railway_stations, through: :railway_stations_routes
-  has_many :trains  
+  has_many :trains
 
   validates :name, presence: true
 
   def self.available_routes(arrival_station_id, departure_station_id)
-    chosen_routes = []
-    Route.all.each do |route|
-      if route.stations_in_route?(arrival_station_id, departure_station_id)
-        chosen_routes << route
-      end
-    end  
-    chosen_routes
-  end
-
-  def stations_in_route?(arrival_station_id, departure_station_id)    
-    arrival_station_in_route = false
-    departure_station_in_route = false
-    arrival_in_route = self.railway_stations.where(id: arrival_station_id.to_i).ids[0]
-    departure_in_route = self.railway_stations.where(id: departure_station_id.to_i).ids[0]
-    arrival_in_route && departure_in_route
+    Route.joins(:railway_stations_routes).where(railway_stations_routes: { railway_station_id: arrival_station_id }) &
+    Route.joins(:railway_stations_routes).where(railway_stations_routes: { railway_station_id: departure_station_id })
   end
 end
