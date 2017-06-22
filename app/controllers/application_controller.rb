e12b9f1d03f97e1feb86f0d02383a1054a2c7b08
lokideos <archmage09@gmail.com>
2017-06-22 14:23:06 +0300

    Get rid of excessive method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,18 +16,6 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
   end
-
-  def store_action
-    return unless request.get? 
-    if (request.path != "/users/sign_in" &&
-        request.path != "/users/sign_up" &&
-        request.path != "/users/password/new" &&
-        request.path != "/users/password/edit" &&
-        request.path != "/users/confirmation" &&
-        request.path != "/users/sign_out" &&
-        !request.xhr?) # don't store ajax calls
-      store_location_for(:user, request.fullpath)
-    end
-  end
+  
 end
  
