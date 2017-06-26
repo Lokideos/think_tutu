f67536ce52e94d2e0ae076de2ddd3b560ea74572
lokideos <archmage09@gmail.com>
2017-06-26 21:39:37 +0300

    Fix bad migration

--- db/migrate/20170626175742_fix_sqlite_to_posgresql_migration_bugs.rb
+++ db/migrate/20170626175742_fix_sqlite_to_posgresql_migration_bugs.rb
@@ -1,6 +1,5 @@
 class FixSqliteToPosgresqlMigrationBugs < ActiveRecord::Migration[5.0]
-  def change
-    remove_column :railway_stations_routes, :station_in_route_number, :integer
+  def change    
     add_column :railway_stations_routes, :position, :integer
     add_column :railway_stations_routes, :arrival_time, :datetime
     add_column :railway_stations_routes, :departure_time, :datetime
