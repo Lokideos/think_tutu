11e2ea468c3bcf965c114791d58e30d7053c6079
lokideos <archmage09@gmail.com>
2017-06-26 13:42:22 +0300

    Fix station - train association

--- app/models/railway_station.rb
+++ app/models/railway_station.rb
@@ -1,5 +1,5 @@
 class RailwayStation < ApplicationRecord
-  has_many :trains
+  has_many :trains, foreign_key: :railway_station_id, class_name: 'Train'
 
   has_many :first_station_tickets, class_name: 'Ticket', foreign_key: :first_station_ticket_id
   has_many :last_station_tickets, class_name: 'Ticket', foreign_key: :last_station_ticket_id
